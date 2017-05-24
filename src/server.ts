fe3a09e806b2a4f896904490de0b5f92df637269
Hillyer <shillyer@mgc.mentorg.com>
2017-05-24 15:04:45 -0700

    Start user login logic

--- src/server.ts
+++ src/server.ts
@@ -16,15 +16,29 @@ app.listen(serverPort, () => {
 
 // Routes
 app.get('/', function (req, res) {
+    // For now we just send a static page with embedded js that allows user to go back and forth
+    // TODO: Make this render a REACT component composed of the login screen and boilerplate template
     res.sendFile(__dirname + '/index.html');
 });
 
 
 
-// Listening with socket.io
+
+// Listening with socket.io to all connections and handling the logic
+/* TODO:
+ * 1. 'username message' handling. Add user to list of users?
+ * 2. When user logs in, add "user 'x' has logged in" to chat history in local storage
+ * 3. When chat message is received,  add it to the message history in local storage
+ * 4. In either event, how do we notify the other clients about what's happened?
+ *
+ */
 io.on('connection', function (socket) {
     console.log('user connected');
     
+    socket.on('user login', function (username: string) {
+        console.log('Login request: ' + username);
+    });
+
     socket.on('chat message', function (msg: string) {
         console.log('Message: ' + msg);
         io.emit('chat message', msg);
@@ -36,3 +50,4 @@ io.on('connection', function (socket) {
 });
 
 server.listen(8080);
+
